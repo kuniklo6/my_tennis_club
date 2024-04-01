756d2b76940ad66d536d7a77ad2b2c55c2eba184
Frank Conejo <kuniklo.vt@gmail.com>
2024-04-01 15:49:41 +0100

    .

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k#88wb)qn#1)pics@#x&xg3g0o^s5!&a_83_yy(pi=ilb$ru18'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
